102d9a478911102b6700232c2a77e25d71e3c158
Bruno Rossi <bruno.gavioli@etec.sp.gov.br>
2020-11-29 11:53:06 -0300

    primeiro commit

---
+++ Clinica-DS+BDI+MVC/Models/AnimalModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinica_DS_BDI_MVC.Models
+{
+    [Table("Animal")]
+    public class AnimalModel
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Nome { get; set; }
+
+        public string Peso { get; set; }
+
+        public string Altura { get; set; }
+        
+        public string Comprimento { get; set; }
+
+        public string Pedigree { get; set; }
+
+
+    }
+}
---
+++ Clinica-DS+BDI+MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Clinica_DS_BDI_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
